93c04f8d589f1b6933f4e599f64f593e70a29cd9
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-12-17 18:54:56 +0300

    update loki

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Sesames.Mixfile do
   defp deps do
     [{:ex_doc, ">= 0.0.0", only: :dev},
      {:inflex, "~> 1.7.0" },
-     {:loki, "~> 1.0.1"},
+     {:loki, "~> 1.1.0"},
      {:phoenix, "~> 1.2.1"}]
   end
 end
