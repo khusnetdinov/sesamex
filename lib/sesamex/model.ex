56e78058e48977cc5870b8e76790ce93ca3a2a28
Marat Khusnetdinov <marat@khusnetdinov.ru>
2017-01-10 15:23:22 +0300

    model

---
+++ lib/sesamex/model.ex
@@ -0,0 +1,57 @@
+defmodule Sesamex.Model do
+  import Ecto.Changeset
+  import Sesamex.Model.Validation
+
+
+  @moduledoc """
+  """
+
+
+  @doc """
+  """
+  def changeset(model, params \\ :empty) do
+    changeset(model, params, [:email])
+  end
+
+  def changeset(model, params, opts) do
+    cast_fields = Keyword.fetch!(opts, :cast)
+
+    model
+    |> cast(params, cast_fields)
+    |> unique_constraint(:email)
+  end
+
+
+  @doc """
+  """
+  def registration_changeset(model, params) do
+    registration_changeset(model, params, password_length: [min: 6, max: 100])
+  end
+
+  def registration_changeset(model, params, opts) do
+    password_length = Keyword.fetch!(opts, :password_length)
+
+    model
+    |> changeset(params)
+    |> cast(params, [:password, :password_confirmation])
+    |> validate_length(:password, password_length)
+    |> validate_password_confirmation
+    |> encrypt_password
+  end
+
+
+  defp encrypt_password(model) do
+    case model do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(model, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        model
+    end
+  end
+
+  defmacro __using__([]) do
+    quote do
+      import Sesamex.Model
+    end
+  end
+end
---
+++ lib/sesamex/model/validation.ex
@@ -0,0 +1,32 @@
+defmodule Sesamex.Model.Validation do
+  import Ecto.Changeset
+
+
+  @moduledoc """
+  """
+
+
+  @doc """
+  """
+  def validate_password_confirmation(changeset) do
+    validate_password_confirmation(changeset, "Invalid email or password.")
+  end
+
+  def validate_password_confirmation(changeset, error_message) do
+    password = get_field(changeset, :password)
+    password_confirmation = get_field(changeset, :password_confirmation)
+
+    if password_confirmation == password do
+      changeset
+    else
+      add_error(changeset, :password_confirmation, error_message)
+    end
+  end
+
+
+  defmacro __using__([]) do
+    quote do
+      import Sesamex.Model.Validation
+    end
+  end
+end
